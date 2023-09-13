d0cc8cd2027839fa917c184eee721da31cd9ef78
Pedro Penteado <106943485+Dreppo@users.noreply.github.com>
2023-09-13 15:38:06 -0300

    update no código para que o json esteja em UTF-8

--- consumo.py
+++ consumo.py
@@ -1,6 +1,13 @@
 import json
 
-with open('Consumo_API\json\world_universities_and_domains.json', 'r') as arquivo:
+with open('Consumo_API\json\world_universities_and_domains.json', 'r', encoding='utf-8') as arquivo:
     dados_json = json.load(arquivo)
-    
-print(dados_json)
+
+for universidade in dados_json:
+    print("Nome da Universidade:", universidade["name"])
+    print("Domínios:", universidade["domains"])
+    print("Páginas da Web:", universidade["web_pages"])
+    print("País:", universidade["country"])
+    print("Código Alfa-2:", universidade["alpha_two_code"])
+    print("Estado/Província:", universidade["state-province"])
+    print()
