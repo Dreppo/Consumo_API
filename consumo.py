e50e933021e5b1a5c8aa0f7f1083507b888ad77b
Pedro Penteado <pedrobbp22@gmail.com>
2023-09-14 12:59:34 -0300

    Update consumo.py

--- consumo.py
+++ consumo.py
@@ -1,13 +1,39 @@
 import json
+import webbrowser
 
 with open('Consumo_API\json\world_universities_and_domains.json', 'r', encoding='utf-8') as arquivo:
     dados_json = json.load(arquivo)
 
-for universidade in dados_json:
-    print("Nome da Universidade:", universidade["name"])
-    print("Domínios:", universidade["domains"])
-    print("Páginas da Web:", universidade["web_pages"])
-    print("País:", universidade["country"])
-    print("Código Alfa-2:", universidade["alpha_two_code"])
-    print("Estado/Província:", universidade["state-province"])
-    print()
+def verificacao_de_escolha(num1,num2):
+    if escolha < num1:
+        print("Digite um número valido")   
+    elif escolha > num2:
+        print("Digite um número valido")
+        
+
+def menu():
+    opc = 0
+    while opc != 4:
+        print("Bem vindo a busca por universidades pelo mundo")
+        print("----------------------------------------------")
+        print("1 - Procurar universdades pelo país")
+        print("2 - Procurar universidades pelo nome")
+        print("3 - Procurar site de uma universidade")
+        print("4 - Sair")
+        print("----------------------------------------------")
+        escolha = int(input("digite um número para fazer uma ação: "))
+        opc = escolha
+        print("")        
+
+def escolha1(escolha):
+    opc = 0
+    while opc != 3:
+        print("1 - Procurar Pelo nome do Pais")
+        print("2 - Procurar Pela sigla do Pais  Ex: BR(Brasil)")
+        print("3 - Voltar")
+    
+
+
+
+
+menu()
