44eef685edd6ac66ae904cbbbe4a0a5a8f5e30fd
Pedro Penteado <106943485+Dreppo@users.noreply.github.com>
2023-09-15 21:57:44 -0300

    Update consumo.py

--- consumo.py
+++ consumo.py
@@ -1,19 +1,13 @@
 import json
 import webbrowser
 
-with open('Consumo_API\json\world_universities_and_domains.json', 'r', encoding='utf-8') as arquivo:
+with open('json\world_universities_and_domains.json', 'r', encoding='utf-8') as arquivo:
     dados_json = json.load(arquivo)
 
 lista_nomes = []
 lista_siglas = []
 dic_exemplo1 = {}
 dic_exemplo2 = {}
-
-def verificacao_de_escolha(num1,num2,esco):
-    if esco < num1:
-        print("Digite um número valido")   
-    elif esco > num2:
-        print("Digite um número valido")
 
 def lista_n():
     for universidade in dados_json:
@@ -26,36 +20,25 @@ def lista_s():
     for universidade in dados_json:
         if universidade["alpha_two_code"] not in lista_siglas:
             lista_siglas.append(universidade["alpha_two_code"])
-    
     print(lista_siglas)
     
 def mostrar_dic1(str1 , x):
     for universidade in dados_json:
         if universidade["country"] == str1:
             x = x + 1
-           
-            dic_exemplo1["id"] = x
-            dic_exemplo1["name"] = universidade["name"]
-            dic_exemplo1["web_pages"] = universidade["web_pages"]
-            print(dic_exemplo1)
+            dic_exemplo1[x] = {x:[universidade["name"],universidade["web_pages"]]}
+    for itens in dic_exemplo1.values():
+        print(itens)           
             
-def mostrar_dic(x):
+def mostrar_dic2(str2,x):
     for universidade in dados_json:
-        x = x + 1  
-        dic_exemplo1["id"] = x
-        dic_exemplo1["name"] = universidade["name"]
-        dic_exemplo1["web_pages"] = universidade["web_pages"]
-        print(dic_exemplo1)
-
-def item_dic(id):
-    for item in dic_exemplo1["id"]:
-        if dic_exemplo1["id"] == id:
-            print(dic_exemplo1[item])
-            link =  dic_exemplo1["web_pages"]
-            webbrowser.open(link)
-
-              
-
+        if universidade["alpha_two_code"] == str2:
+            x = x + 1
+            dic_exemplo2[x] = {x:[universidade["name"],universidade["web_pages"]]}
+    for itens in dic_exemplo2.values():
+        print(itens) 
+    
+    
 def escolha1():
     opc = 0
     while opc != 3:
@@ -71,20 +54,34 @@ def escolha1():
                 if busca not in lista_nomes:
                     print("Digite um nome valido para a pesquisa")
                 mostrar_dic1(busca, 0)
-                id = int(input("Digite o ID da Universidade que deseja ver: "))  
-                item_dic(id) 
+                id = int(input("Digite o Número da Universidade que deseja ver: "))  
+                mostrar_escolha(dic_exemplo1,id) 
                 print()
             case 2:
                 lista_s()
                 aux = str(input("Digite a sigla do país: "))
                 busca = aux.upper()
                 if busca not in lista_siglas:
-                    print("Digite uma sigla valida para a pesquisa")
+                    print("Digite uma sigla valida para a pesquisa: ")
+                else:
+                    mostrar_dic2(busca, 0)
+                    id = int(input("Digite o Número da Universidade que deseja ver: "))  
+                    mostrar_escolha(id,dic_exemplo2) 
+             
                 
 
-def escolha2():    
-    busca = str(input("Digite o nome da Universidade"))
-    busca         
+def escolha2(str2):
+    for universidade in dados_json:
+        if universidade["name"] == str2:
+                print (universidade["name"])
+                print (universidade["country"])
+                print (universidade["alpha_two_code"])
+        else:
+            print("Universidade não encontrada")
+            break
+
+        
+             
                 
                 
         
@@ -107,7 +104,8 @@ def menu():
             case 1:
                 escolha1()
             case 2:
-                escolha2()
+                busca = str(input("Digite o nome da Universidade: "))
+                escolha2(busca)
                 
         print("")        
 
